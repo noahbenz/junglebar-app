45ee44fc8c227c360927adb0c691eba6d8fd00bc
Valentin Domenic Morgenthaler <valentin.morgenthaler@edu.tbz.ch>
2023-12-22 14:20:00 +0000

    Revert "Merged PR 2: fix: banner size"

--- junglebar/src/app/app.routes.ts
+++ junglebar/src/app/app.routes.ts
@@ -1,22 +1,3 @@
-import { RouterModule, Routes } from '@angular/router';
-import { NgModule } from '@angular/core';
-import { LandingpageComponent } from './components/landingpage/landingpage.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  { path: '', component: LandingpageComponent },
-  // { path: 'drinks', component: DrinksComponent },
-  // { path: 'book', component: BookComponent },
-  // { path: 'contact', component: ContactComponent },
-  // { path: 'profile', component: ProfileComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent }
-
-];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
+export const routes: Routes = [];
