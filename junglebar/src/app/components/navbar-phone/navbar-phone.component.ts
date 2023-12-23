d4c782210b51429cdc4d683e89c6e8508c1bfff0
Noah Benz <noah.benz@edu.tbz.ch>
2023-12-23 15:06:39 +0100

    wip: navbar on phone

--- junglebar/src/app/components/navbar-phone/navbar-phone.component.ts
+++ junglebar/src/app/components/navbar-phone/navbar-phone.component.ts
@@ -1,12 +1,26 @@
+import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLinkActive, RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar-phone',
   standalone: true,
-  imports: [],
+  imports: [RouterLinkActive, RouterModule, NgIf, NgClass],
   templateUrl: './navbar-phone.component.html',
   styleUrl: './navbar-phone.component.scss'
 })
 export class NavbarPhoneComponent {
+  isMenuOpen: boolean = false;
+  iconSrc: string = 'assets/svg/burger-icon.svg'
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+
+    if (this.isMenuOpen) {
+      this.iconSrc = 'assets/svg/burger-icon-opened.svg';
+    } else {
+      this.iconSrc = 'assets/svg/burger-icon.svg';
+    }
+  }
 }
