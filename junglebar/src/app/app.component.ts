60f3776d99331cc2232da25a8b24b447394bcfd2
Fanzun Jann <110892321+jannfanzun@users.noreply.github.com>
2023-12-24 15:53:09 +0100

    wip: about-us section

--- junglebar/src/app/app.component.ts
+++ junglebar/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { AboutUsComponent } from './components/about-us/about-us.component';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
@@ -7,7 +8,7 @@ import { NavbarComponent } from './components/navbar-laptop/navbar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LandingpageComponent, NavbarComponent, RouterModule],
+  imports: [CommonModule, RouterOutlet, LandingpageComponent, NavbarComponent, RouterModule, AboutUsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
