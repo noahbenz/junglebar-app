870e2cee1cc2bf6ad60467a11ddd8b42297c5634
Fanzun Jann <110892321+jannfanzun@users.noreply.github.com>
2024-01-11 11:21:05 +0100

    feat: add fruit divider

--- junglebar/src/app/components/landingpage/landingpage.component.ts
+++ junglebar/src/app/components/landingpage/landingpage.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar-laptop/navbar.component';
 import { NavbarPhoneComponent } from '../navbar-phone/navbar-phone.component';
 import AboutUsComponent from '../about-us/about-us.component';
+import { FruitDividerComponent } from '../fruit-divider/fruit-divider.component';
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [NavbarComponent, NavbarPhoneComponent, AboutUsComponent],
+  imports: [NavbarComponent, NavbarPhoneComponent, AboutUsComponent, FruitDividerComponent],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.scss'
 })
