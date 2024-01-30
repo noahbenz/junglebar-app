9d44845f6ee41069ed26ac2a02510c46546d3b72
Noah Benz <noah.benz@edu.tbz.ch>
2024-01-30 15:04:45 +0100

    wip: add favorise drinks

--- junglebar/src/app/components/favorite-drinks/favorite-drinks.component.ts
+++ junglebar/src/app/components/favorite-drinks/favorite-drinks.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-favorite-drinks',
@@ -8,11 +8,41 @@ import { Component, EventEmitter, Output } from '@angular/core';
   templateUrl: './favorite-drinks.component.html',
   styleUrls: ['./favorite-drinks.component.scss']
 })
-export class FavoriteDrinksComponent {
+export class FavoriteDrinksComponent implements OnInit {
   @Output() closeModalEvent = new EventEmitter();
+  favoriteDrinks: any[] = [];
+
+  ngOnInit(): void {
+    // Lese die gespeicherten Favoriten-Drinks aus dem Local Storage
+    const storedFavoriteDrinksJSON = localStorage.getItem('favoriteDrinks');
+    this.favoriteDrinks = JSON.parse(storedFavoriteDrinksJSON || '[]');
+  }
 
   closeModal() {
-    console.log("drink");
     this.closeModalEvent.emit();
+  }
+
+  toggleFavorite(drink: any) {
+    drink.isFavorited = !drink.isFavorited;
+
+    // Hole zuerst die gespeicherten Favoriten aus dem Local Storage
+    let favoriteDrinks: any[] = JSON.parse(localStorage.getItem('favoriteDrinks') || '[]');
+
+    // Überprüfe, ob der Drink bereits in den Favoriten ist
+    const index = favoriteDrinks.findIndex((favDrink) => favDrink.name === drink.name);
+
+    if (drink.isFavorited) {
+      // Wenn der Drink nicht in den Favoriten ist, füge ihn hinzu
+      if (index === -1) {
+        favoriteDrinks.push(drink);
+      }
+    } else {
+      // Wenn der Drink in den Favoriten ist, entferne ihn
+      if (index !== -1) {
+        favoriteDrinks.splice(index, 1);
+      }
+    }
+
+    localStorage.setItem('favoriteDrinks', JSON.stringify(favoriteDrinks));
   }
 }
